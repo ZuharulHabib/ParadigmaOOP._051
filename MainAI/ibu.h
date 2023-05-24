599258073f27dbad048f036c5f409dc8d9a78699
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2023-05-24 19:08:30 +0700

    menambahkan ibu dan daftar anak

--- MainAI/ibu.h
+++ MainAI/ibu.h
@@ -18,3 +18,13 @@ public:
 };
 void ibu::tambahAnak(anak* pAnak) {
 	daftar_anak.push_back(pAnak);
+
+}
+void ibu::cetakAnak() {
+	cout << "\nDaftar Anak dari ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif
